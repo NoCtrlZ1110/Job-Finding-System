6ca6e33e2a8d9e50c3e8667a62af48b24ba4f9ab
NoCtrlZ <18020651@vnu.edu.vn>
2020-04-21 03:14:27 +0700

    Employee

--- src/components/Employee/CreateProfile/CreateProfile.tsx
+++ src/components/Employee/CreateProfile/CreateProfile.tsx
@@ -1,4 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import Modal from "react-bootstrap/Modal";
+
 export const CreateProfile: React.FC = () => {
-  return <div>Create profile here!</div>;
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ color: "#2665b8" }}>
+              <Card.Header>
+                <strong>
+                  <h5>
+                    <b>TẠO HỒ SƠ VIỆC LÀM</b>
+                  </h5>
+                </strong>
+              </Card.Header>
+              <Card.Body style={{ textAlign: "left" }}>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>
+                      <b>Họ và Tên</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập tên của bạn"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>
+                      <b>Số điện thoại</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập số điện thoại"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>
+                      <b>Email</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập email của bạn"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>
+                      <b>Địa Chỉ</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập địa chỉ"
+                    />
+                  </Form.Group>
+
+                  <div className="d-flex flex-row-reverse">
+                    <Button
+                      className="btn-md my-3 mx-2"
+                      variant="primary"
+                      onClick={handleShow}
+                    >
+                      <b>Tạo hồ sơ</b>
+                      <FontAwesomeIcon className="ml-2" icon={faPlusCircle} />
+                    </Button>
+                  </div>
+                  <Row>
+                    <Col xs={10} />
+                  </Row>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <Modal show={show} onHide={handleClose} centered size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Tạo hồ sơ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2>Đã tạo hồ sơ thành công!</h2>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
--- src/components/Employee/Find/FindJob.tsx
+++ src/components/Employee/Find/FindJob.tsx
@@ -1,26 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export const FindJob: React.FC = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card style={{ height: "40em" }}>
-            <Card.Header>
-              <strong>
-                <h5>TÌM KIẾM VIỆC LÀM</h5>
-              </strong>
-            </Card.Header>
-            <Card.Body>
-              This is some text within a card
-              body.asfduyisdhfiaskdjfhasukdfhadsuifkashdfkuasjdfhauskdjfhasdfuk
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ color: "#2665b8" }}>
+              <Card.Header>
+                <strong>
+                  <h5>
+                    <b>TÌM KIẾM VIỆC LÀM</b>
+                  </h5>
+                </strong>
+              </Card.Header>
+              <Card.Body style={{ textAlign: "left" }}>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>
+                      <b>Khu vực</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập khu vực bạn muốn tìm việc"
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>
+                      <b>Loại công việc</b>
+                    </Form.Label>
+                    <Form.Control as="select">
+                      <option>Công nghệ thông tin</option>
+                      <option>Thiết kế đồ hoạ</option>
+                      <option>Sửa ống nước</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Row className="mb-3">
+                    <Col>
+                      <label className="col-form-label">
+                        <b>Từ</b>
+                      </label>
+                      <input className="form-control col-10" type="date" />
+                    </Col>
+                    <Col>
+                      <label className="col-form-label">
+                        <b>Đến</b>
+                      </label>
+                      <input className="form-control " type="date" />
+                    </Col>
+                  </Row>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>
+                      <b>Mức lương mong muốn</b>
+                    </Form.Label>
+                    <Form.Control
+                      type="destination"
+                      placeholder="Nhập khu vực bạn muốn tìm việc"
+                    />
+                  </Form.Group>
+
+                  <div className="d-flex flex-row-reverse">
+                    <Button
+                      className="btn-md my-3 mx-2"
+                      variant="primary"
+                      // type="submit"
+                      onClick={handleShow}
+                    >
+                      <b>Tìm kiếm</b>
+                      <FontAwesomeIcon className="ml-2" icon={faSearch} />
+                    </Button>
+                  </div>
+                  <Row>
+                    <Col xs={10} />
+                  </Row>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Modal show={show} onHide={handleClose} centered size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Kết quả tìm kiếm</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="search">
+          <h2>Woohoo, không tìm thấy kết quả nào!</h2>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
